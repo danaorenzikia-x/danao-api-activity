47009d60d26fad7563757e56266f9f6ad8c12771
danaorenzikia-x <danao.renzikia@marsu.edu.ph>
2026-02-05 10:09:43 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+//server.js
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./src/config/db');
+const apiRoutes = require('./src/routes/apiroutes');
+const app = express();
+connectDB();
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// Load Config
+const PORT =process.env.PORT || 3000;
+const BASE_URI = process.env.BASE_URI || '/api/v1';
+
+//const apiRoutes = require('./src/routes/apiRoute');
+app.use(process.env.BASE_URI, apiRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+    console.log(`Base URI: http://localhost:${PORT}${BASE_URI}`);
+});
